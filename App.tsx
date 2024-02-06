fc95cd96684c2579ffc1a808a79117a1c8d5bbc7
Kelvin Wong <kelvin.wong@netint.ca>
2024-02-05 16:38:56 -0800

    add tailwind

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <View className="items-center justify-center flex-1 bg-white">
+      <Text>Open up App.js to start working on your app!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
